d8283aefe900e52f45692f7a8865e7a42a8f1a8b
Alice <34670205+AliceCSY@users.noreply.github.com>
2019-02-24 02:08:28 -0500

    update

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors());
 app.options("*", cors());
 
-var google = require('./google.js');
-var reddit = require('../redditScraper/index.js');
+const google = require('./google.js');
+const reddit = require('../redditScraper/index.js');
 
 
 app.get('/', function (req, res) {
@@ -69,15 +69,14 @@ async function getData(subReddits) {
 }
 
 /**
-* combineDuplicates(array): Merges all the elements in the data array with the same title.
+* combineDuplicates(dataArray): Merges all the elements in the dataArray with the same title.
 */
-function combineDuplicates(array) {
+function combineDuplicates(dataArray) {
 	let newArray = [];
 
-	while (array.length > 0) {
-		let currentTitle = array[0].text;
-
-		let curTitleArray = array.filter((a) => a.text === currentTitle);
+	while (dataArray.length > 0) {
+		let currentTitle = dataArray[0].text;
+		let curTitleArray = dataArray.filter((a) => a.text === currentTitle);
 
 		if (curTitleArray.length > 1) {
 			let linkList = [];
@@ -88,11 +87,11 @@ function combineDuplicates(array) {
 			newArray.push({text: currentTitle, size: linkList.length * 25, href: linkList});
 			console.log({text: currentTitle, size: linkList.length * 25, href: linkList});
 		} else {
-			newArray.push({text: currentTitle, size: 1 * 25, href: array[0].href[0]});
-			console.log({text: currentTitle, size: 1 * 25, href: array[0].href[0]});
+			newArray.push({text: currentTitle, size: 1 * 25, href: dataArray[0].href[0]});
+			console.log({text: currentTitle, size: 1 * 25, href: dataArray[0].href[0]});
 		}
 
-		array = array.filter((a) => a.text !== currentTitle);
+		dataArray = dataArray.filter((a) => a.text !== currentTitle);
 	}
 
 	return newArray;
