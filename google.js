8ae9b7ef2c1e6158413b0fd68eaf74e98de3dc23
Ian <iangu234@gmail.com>
2019-05-01 23:29:28 -0400

    auth

--- google.js
+++ google.js
@@ -1,5 +1,21 @@
 // Imports the Google Cloud client library
 const language = require('@google-cloud/language');
+const GoogleAuth = require('google-auth-library');
+
+//google auth
+function authorize() {
+    return new Promise(resolve => {
+        const authFactory = new GoogleAuth();
+        const jwtClient = new authFactory.JWT(
+            process.env.GOOGLE_CLIENT_EMAIL, // defined in Heroku
+            null,
+            process.env.GOOGLE_PRIVATE_KEY, // defined in Heroku
+            ['https://www.googleapis.com/auth/calendar']
+        );
+
+        jwtClient.authorize(() => resolve(jwtClient));
+    });
+}
 
 // Creates a client
 const client = new language.LanguageServiceClient();
